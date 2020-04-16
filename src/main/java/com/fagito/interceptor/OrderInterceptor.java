b49e06b5250d0542b2058aca1bf3a0e754a8c476
AmlAlx007 <amlalx007@gmail.com>
2020-04-16 18:47:47 +0100

    Interceptor

--- src/main/java/com/fagito/interceptor/OrderInterceptor.java
+++ src/main/java/com/fagito/interceptor/OrderInterceptor.java
@@ -31,7 +31,7 @@ public class OrderInterceptor implements HandlerInterceptor {
 			throws Exception {
 		
 		
-		String str,wholeStr="";
+		String str,wholeStr="",result="";
 		BufferedReader br=request.getReader();
 		
 		while ((str = br.readLine()) != null) {
@@ -45,14 +45,20 @@ public class OrderInterceptor implements HandlerInterceptor {
 		OkHttpClient client = new OkHttpClient().newBuilder()
 				  .build();
 				Request httpRequest = new Request.Builder()
-				  .url("https://api.mailgun.net/v4/address/validate?address="+jsonObject.get("email"))
+				  .url("https://api.mailgun.net/v4/address/validate?address=".concat(jsonObject.get("email").toString().replace("\"","")))
 				  .method("GET", null)
 				  .addHeader("Authorization", "Basic YXBpOmU0Njk3NjJlMmViZDRjMjdlZTNlYzA3ZDQ1MGQyNjU0LTkxNTE2MWI3LTVjMjc1ODE5")
 				  .build();
 				Response httpResponse = client.newCall(httpRequest).execute();
 		
+		result=httpResponse.body().string();
+		jsonObject = new JsonParser().parse(result).getAsJsonObject();
+		if(jsonObject.get("result").toString().replace("\"", "").compareTo("deliverable")!=0)
+		{
+			response.getWriter().write("Email is not correct");
+			return false;
+		}
 		
-		System.out.println(httpResponse);
 		
 		return true;
 		/*System.out.println(request);
